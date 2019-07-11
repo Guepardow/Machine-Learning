21159f06aa05c466047c6573f6a91af6dcb6a9c4
Ragib Ahashan <ahashanragib@gmail.com>
2019-07-11 16:30:42 +0600

    Updates on NNs

--- First Neural Network/MultiLayered_NN.py
+++ First Neural Network/MultiLayered_NN.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 # Activation function will be sigmoid
 def sigmoid(x, derivative = False):
@@ -11,10 +12,19 @@ def sigmoid(x, derivative = False):
 def deriveSigmoid(x):
 	return sigmoid(x)*(1 - sigmoid(x))
 
+def backward_sigmoid(z):
+	return 0-(np.log( (1/z) - 1))
+
+def accuracy_output(nn_output, desired_output_):
+	a1 = ( abs(nn_output[0]-desired_output_[0]))
+	a2 = (abs(nn_output[1]-desired_output_[1]))
+	a3 = ( abs(nn_output[2]-desired_output_[2]))
+	return (a1+a2+a3)/3, a1, a2, a3
+
+
 def Cost(last_layer_neurons_vector, desired_output_vector):
 	cost = 0
 	for i in range(len(last_layer_neurons_vector)):
-		#print('(', last_layer_neurons_vector[i], '-', desired_output_vector[i], ")^2   +", end='')
 		cost += (last_layer_neurons_vector[i] - desired_output_vector[i])**2
 	return cost
 
@@ -32,25 +42,14 @@ def generate3DMatrix(m,n,k):
 # nomber of hidden layers = 2
 # nomber of neurons in each hidden layer = 4
 
-# How to make a 2D array/list/matrix (m x n):
-# hidden_neurons = [[0]*nomber_of_neurons_in_one_hidden_layer for i in range(nomber_of_hidden_layers)]
-
-#matrix_neurons = np.array(generate2DMatrix(3,4))
-#print("Initializing neuron matrix...")
-#print(matrix_neurons)
-#print("")
-
 
 def generateRandomWeights(mat_neurons):
 	Layers = len(mat_neurons[0])
 	synapses_layers = Layers - 1
 	Weights = generate3DMatrix(len(mat_neurons[:,0]), len(mat_neurons[:,0]), synapses_layers)
-
-	#Weights.append(generate2DMatrix(len(mat_neurons[i+1,:]), len(mat_neurons[i,:]), 1))
 	return Layers, synapses_layers, Weights
 
 
-#layers, syn_layers, Weights = generateRandomWeights(matrix_neurons)
 
 Weights = np.array(
 				  [[[0.16, 0.71, 0.24],
@@ -82,8 +81,8 @@ print("")
 
 
 #Inputs and outputs:
-inputs = [1,0,1]
-desired_outputs = [1,1,0]
+inputs = [1,1,0]
+desired_outputs = [1, 0, 0]
 matrix_neurons[:,0] = inputs
 
 print("Initializing neuron matrix with random values...")
@@ -91,7 +90,7 @@ print(matrix_neurons)
 print("")
 
 print("Initializing random bias...")
-Bias = [0.32, 0.5, 1]#[random.randint(0,100)/100 for k in range(3)]
+Bias = [0.32, 0.5, 1] 					#[random.randint(0,100)/100 for k in range(3)]
 print(Bias)
 print("")
 
@@ -128,12 +127,15 @@ forwardPropagation()
 
 print("The values in the neurons are currently...")
 print(matrix_neurons)
+print("")
 
 
 # Backpropagation algorithm
 def derivative_cost_weight(layer, last_layer_neuron, current_layer_neuron):
 	total_cost = Cost(matrix_neurons[:,layers-1] , desired_outputs)
-	return 2 * (matrix_neurons[last_layer_neuron][layer - 1]) * sigmoid(matrix_neurons[current_layer_neuron][layer], True) * total_cost
+	#z = backward_sigmoid(matrix_neurons[current_layer_neuron][layer])
+	z = matrix_neurons[current_layer_neuron][layer]*(1 - matrix_neurons[current_layer_neuron][layer])
+	return 2 * (matrix_neurons[last_layer_neuron][layer - 1]) * z * total_cost
 
 def derivative_cost_bias(layer):
 	cost = Cost(matrix_neurons[:,layers-1] , desired_outputs)
@@ -141,25 +143,22 @@ def derivative_cost_bias(layer):
 	for neuron in range(len(matrix_neurons[:,layer])):
 		sum += neuron
 	average_value_in_layer = sum/len(matrix_neurons[:,layer])
-	return 2*sigmoid(average_value_in_layer, True) * cost
+	#z = backward_sigmoid(average_value_in_layer)
+	z = average_value_in_layer*(1-average_value_in_layer)
+	return 2 * z * cost
 
-def derivative_cost_neuron(layer, nNeuron):
-	pass
 
 def backpropagation(alpha = 0.01):
 	# Loop that goes through all layers backwards
 	for layer in reversed(range(1,layers)):
-		b = derivative_cost_bias(layer)
-		Bias[layer-1] = Bias[layer-1] - alpha*b
+		Wb = derivative_cost_bias(layer)
+		Bias[layer-1] = Bias[layer-1] - alpha*Wb
 		for current_layer_neuron in range(len(matrix_neurons[:,layer])):
-			print(Weights)
-			print("")
-			print("")
 			for last_layer_neuron in range(len(matrix_neurons[:,layer-1])):
-				d = derivative_cost_weight(layer, last_layer_neuron, current_layer_neuron)
-				w_jk = Weights[current_layer_neuron][last_layer_neuron][layer-1]
-				w_jk = w_jk - alpha * d
-				Weights[last_layer_neuron][current_layer_neuron][layer-1] = w_jk
+				dW = derivative_cost_weight(layer, last_layer_neuron, current_layer_neuron)
+				w_jk = Weights[layer-1][current_layer_neuron][last_layer_neuron]
+				w_jk = w_jk - alpha * dW
+				Weights[layer-1][current_layer_neuron][last_layer_neuron] = w_jk
 				
 
 
@@ -167,42 +166,84 @@ def backpropagation(alpha = 0.01):
 
 def training(W = Weights, matrix = matrix_neurons, b = Bias, alpha = 0.01):
 	#training only the last layer
+	Last_layer_neuron_Position = 0
+	Current_layer_neuron_position = 0
+	
+	for current_layer_neuron in matrix[:, layers - 1]:
+		z = backward_sigmoid(current_layer_neuron)
+		for last_layer_neuron in matrix_neurons[:, layers - 2]:
 
-	for neuron in matrix[:, layers - 1]:
-		print(neuron)
-		neuron = 1
+			dW = 2*last_layer_neuron*deriveSigmoid(z)*Cost(matrix_neurons[:, 3], desired_outputs)
+
+			w_jk = Weights[layers-2][Current_layer_neuron_position][Last_layer_neuron_Position]
+
+			w_jk = w_jk - alpha*dW
+			Weights[layers-2][Current_layer_neuron_position][Last_layer_neuron_Position] = w_jk
+
+			Current_layer_neuron_position += 1
+			if(Current_layer_neuron_position == 3):
+				Current_layer_neuron_position = 0
+
+		Last_layer_neuron_Position = (1+Last_layer_neuron_Position)%3
 
 
 
-def printOutputs(output = desired_outputs):
-	print("Desired outputs: ")
-	print(output)
-	print("")
+newCost = Cost(matrix_neurons[:, 3], desired_outputs)
+prevCost = Cost(matrix_neurons[:, 3], desired_outputs) + 1
 
+J_history = []
+out_1 = []
+out_2 = []
+out_3 = []
 
-def printBias(b = Bias):
-	print("Bias: ", Bias)
-	#print(Bias)
-	print("")
-
-def printNeurons(neurons = matrix_neurons):
-	printBias()
-	print("Neurons: ")
+#for i in range(5000):
+while(prevCost >= newCost):
 	forwardPropagation()
-	print(neurons)
-	print("Cost: ", Cost(matrix_neurons[:, 3], desired_outputs))
+	backpropagation()
+	prevCost = newCost
+	forwardPropagation()
+	newCost = Cost(matrix_neurons[:, 3], desired_outputs)
+
+	out_1.append(matrix_neurons[0][3])
+	out_2.append(matrix_neurons[1][3])
+	out_3.append(matrix_neurons[2][3])
+
+	J_history.append(newCost)
 
 
-
-forwardPropagation()
-#backpropagation()
-training()
-print(matrix_neurons)
-print(Cost(matrix_neurons[:, 3], desired_outputs))
+print("Output layer and the desired outputs")
+print(matrix_neurons[:,3] , "   " ,desired_outputs)
 
 
+x = np.linspace(0,len(J_history),len(J_history))
+
+plt.figure()
+
+# J_History
+plt.subplot(2,2,1)
+plt.plot(x, J_history)
+plt.title('J_history')
+plt.ylabel("Error")
 
 
+# Output 1 history
+plt.subplot(2,2,2)
+plt.plot(x, out_1)
+plt.title('Output 1 history')
 
+
+# Output 1 history
+plt.subplot(2,2,3)
+plt.plot(x, out_2)
+plt.title('Output 2 history')
+
+
+# Output 1 history
+plt.subplot(2,2,4)
+plt.plot(x, out_3)
+plt.title('Output 3 history')
+
+
+plt.show()
 
 
